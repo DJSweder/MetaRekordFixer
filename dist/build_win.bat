faec477ff0edaac9c109915162555f0413ad93d9
DJ Sweder <djsweder@gmail.com>
2025-06-30 08:56:35 +0200

    Fix build script to use right paths.

--- dist/build_win.bat
+++ dist/build_win.bat
@@ -4,23 +4,23 @@ echo Setting the number of processor cores for compilation...
 set GOMAXPROCS=8
 
 echo Changing to src directory...
-cd src
+cd /d "%~dp0..\src"
 
 echo Updating dependencies...
 go mod tidy
 
-echo Changing back to root...
-cd ..
-
 echo Updating metadata and app icon...
+cd /d "%~dp0.."
 go-winres make --in "dist/winres/winres.json"
 
+cd /d "%~dp0..\src"
 echo Compiling the application...
 set CGO_ENABLED=1
 set CGO_CFLAGS=-w
-go build -trimpath -buildvcs=false -ldflags "-w -s -H windowsgui" -o "dist/compilated/metarekordfixer.exe" src/main.go
+go build -trimpath -buildvcs=false -ldflags "-w -s -H windowsgui" -o "../dist/compilated/metarekordfixer.exe" main.go
 
 echo Compressing the final binary file...
+cd /d "%~dp0.."
 upx --best "dist/compilated/metarekordfixer.exe"
 
 echo Compilation completed successfully.
