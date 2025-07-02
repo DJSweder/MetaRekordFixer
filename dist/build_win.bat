0d642cc1cb42805d48c018fa391e18fd4ff59826
DJ Sweder <djsweder@gmail.com>
2025-07-02 07:21:00 +0200

    Fixing build_win.bat

--- dist/build_win.bat
+++ dist/build_win.bat
@@ -3,24 +3,21 @@
 echo Setting the number of processor cores for compilation...
 set GOMAXPROCS=8
 
-echo Cleaning old resource files...
-del /q "%~dp0..\src\*_windows_*.syso"
-
-echo Changing to src directory...
 cd /d "%~dp0..\src"
+
+echo Cleaning old resource files...
+del /q *.syso
+
+echo Generating icon and metadata...
+go-winres make --in "%~dp0..\dist\winres\winres.json" --out "rsrc"
 
 echo Updating dependencies...
 go mod tidy
 
-echo Updating metadata and app icon...
-cd /d "%~dp0.."
-go-winres make --in "dist/winres/winres.json" --out "src/rsrc"
-
-cd /d "%~dp0..\src"
 echo Compiling the application...
 set CGO_ENABLED=1
 set CGO_CFLAGS=-w
-go build -trimpath -buildvcs=false -ldflags "-w -s -H windowsgui" -o "../dist/compilated/metarekordfixer.exe" main.go
+go build -trimpath -buildvcs=false -ldflags "-w -s -H windowsgui" -o "..\dist\compilated\metarekordfixer.exe"
 
 REM echo Compressing the final binary file...
 REM cd /d "%~dp0.."
