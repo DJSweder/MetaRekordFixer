0f705cc798328a1e8ebd800b908c5a0778b97e8d
DJ Sweder <djsweder@gmail.com>
2025-07-02 08:12:42 +0200

    Fixing building script, problem with saving final binary file to destination folder.

--- dist/build_win.bat
+++ dist/build_win.bat
@@ -15,10 +15,17 @@ go-winres make --in "%~dp0..\dist\winres\winres.json" --out "rsrc"
 echo Updating dependencies...
 go mod tidy
 
+cd
+
 echo Compiling the application...
 set CGO_ENABLED=1
 set CGO_CFLAGS=-w
-go build -trimpath -buildvcs=false -ldflags "-w -s -H windowsgui" -o "..\dist\compilated\metarekordfixer.exe"
+go build -trimpath -buildvcs=false -ldflags "-w -s -H windowsgui" -o "../dist/compilated/metarekordfixer.exe"
+if errorlevel 1 (
+    echo BUILD FAILED!
+    exit /b 1
+)
+dir ..\dist\compilated
 
 REM echo Compressing the final binary file...
 REM cd /d "%~dp0.."
