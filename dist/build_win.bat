b648c667033730dfd62b61f5ccfffd567eadad13
DJ Sweder <djsweder@gmail.com>
2025-07-02 09:58:23 +0200

    Cleaning build script.

--- dist/build_win.bat
+++ dist/build_win.bat
@@ -14,28 +14,14 @@ go-winres make --in "%~dp0..\dist\winres\winres.json" --out "rsrc"
 echo Updating dependencies...
 go mod tidy
 
-cd
-
 echo Compiling the application...
 set CGO_ENABLED=1
 set CGO_CFLAGS=-w
 go build -trimpath -buildvcs=false -ldflags "-w -s -H windowsgui" -o "../dist/installer/sources/metarekordfixer.exe"
-if errorlevel 1 (
-    echo BUILD FAILED!
-    exit /b 1
-)
+
 
 cd /d "%~dp0installer"
 echo Creating installer...
-iscc make_install_metarekordfixer-v1.0.0_inn_test.iss
-
-if errorlevel 1 (
-    echo INNO SETUP BUILD FAILED!
-    exit /b 1
-)
-
-cd /d "%~dp0"
-echo --- Release folder content ---
-dir release
+iscc make_install_metarekordfixer-v1.0.0_inno.iss
 
 echo Compilation and installer creation completed successfully.
